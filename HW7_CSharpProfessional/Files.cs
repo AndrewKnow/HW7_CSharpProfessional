0c8fffde41d29ef3314663e3276c853648eb45e0
Андрей Наумов <NaumovAV1983@yandex.ru>
2023-07-28 14:50:31 +0300

    Разработка п2., скоировано решение по п.1

---
+++ HW7_CSharpProfessional/Files.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW7_CSharpProfessional
+{
+    /// <summary>
+    /// Прочитать 3 файла параллельно и вычислить количество пробелов в них (через Task)
+    /// </summary>
+    public static class WorkingWithFiles
+    {
+
+        public static int CountSpacesInFile(string s)
+        {
+            var file1Text = File.ReadAllText(s);
+            var spaceCount = file1Text.Count(it => it == ' ');
+            return spaceCount;
+        }
+
+        public static void CreateFiles(string file1, string file2, string file3)
+        {
+
+            var file1Content = "hello world";
+            var file2Content = "hello great world";
+            var file3Content = "hello funny great world";
+
+            File.WriteAllText(file1, file1Content);
+            File.WriteAllText(file2, file2Content);
+            File.WriteAllText(file3, file3Content);
+        }
+
+        public static void CreateFilesWithRandomWhitespaces(string path)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                FileStream fs = File.Create(path + "\\" + "filename" + i + ".txt");
+                StreamWriter writer = new StreamWriter(fs);
+                Random randomWhitespace = new Random();
+                var countWhitespace = randomWhitespace.Next(1, 100);
+
+                Func<int, string> whiteSpace = x => new string(' ', x);
+
+                writer.Write(whiteSpace(countWhitespace));
+                writer.Close();
+
+                Console.WriteLine($"File {"filename" + i + ".txt"} contains {CountSpacesInFile(path + "\\" + "filename" + i + ".txt")} spaces");
+            }
+        }
+    }
+}
--- HW7_CSharpProfessional/Program.cs
+++ HW7_CSharpProfessional/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace HW7_CSharpProfessional
+﻿using System.IO;
+using static System.Net.WebRequestMethods;
+
+namespace HW7_CSharpProfessional
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Прочитать 3 файла параллельно и вычислить количество пробелов в них (через Task):");
+            var (file1, file2, file3) = ("file1.txt", "file2.txt", "file3.txt");
+
+            WorkingWithFiles.CreateFiles(file1, file2, file3);
+
+            Task task1 = Task.Run(() =>
+            {
+                Console.WriteLine($"File {file1} contains {WorkingWithFiles.CountSpacesInFile(file1)} spaces");
+            });
+            Task task2 = Task.Run(() =>
+            {
+                Console.WriteLine($"File {file2} contains {WorkingWithFiles.CountSpacesInFile(file2)} spaces");
+            });
+            Task task3 = Task.Run(() =>
+            {
+                Console.WriteLine($"File {file3} contains {WorkingWithFiles.CountSpacesInFile(file3)} spaces");
+            });
+
+            Task.WaitAll(task1, task2, task3);
+
+
+            Console.WriteLine("Написать функцию, принимающую в качестве аргумента путь к папке. " +
+                "Из этой папки параллельно прочитать все файлы и вычислить количество пробелов в них.");
+            try
+            {
+                Directory.CreateDirectory("Папка");
+            }
+            catch
+            {
+
+                Console.WriteLine("Папка создана ранее");
+            }
+            
+
+            string path = Directory.GetCurrentDirectory() + @"\Папка";
+
+            WorkingWithFiles.CreateFilesWithRandomWhitespaces(path);
+
+
+
+
+            //Console.ReadKey();
+
+            Console.WriteLine("Finished");
         }
     }
 }
